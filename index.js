5bd8e4b6be8658d21963a0958d72019746c221fb
Temba Sydney Gabriel <tembasydney@gmail.com>
2024-03-27 15:40:24 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply
+let num1 = 31;
+let num2 = 2;
+multiply = num1*num2;
+let random = Math.floor(Math.random()*Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+let mod
+let num3 = 16
+let num4 = 4
+mod = num3/ num4
+ 
+ let max = Math.max (1, 5, 16, 20);
+console.log(max);
